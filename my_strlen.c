2f48d82b89cb5283c5370617b03b7f24bd0f56cb
mik129 <aaxeldiomande@gmail.com>
2026-09-17 15:10:21 +0200

    Update my_strlen.c

--- my_strlen.c
+++ my_strlen.c
@@ -3,11 +3,11 @@
 size_t
 my_strlen(const char *s)
 {
-    size_t n;
+ size_t compteur = 0;
 
-    n = 0;
-    while (s[n] != '\0') {
-        n++;
-    }
-    return n;
+ while (*s != '\0') {
+ compteur += 1;
+ s++;
+ }
+ return compteur;
 }
