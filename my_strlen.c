f824d82c69628b561c55ab181aedbc0697547f19
mik129 <aaxeldiomande@gmail.com>
2026-09-17 11:34:19 +0200

    new

--- my_strlen.c
+++ my_strlen.c
@@ -3,10 +3,10 @@
 size_t
 my_strlen(const char *s)
 {
-        size_t n;
+	size_t n;
 
-        n = 0;
-        while (s[n] != '\0')
-                n++;
-        return n;
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	return n;
 }
