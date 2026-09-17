b0f25c477916b48cded282a7a602b2f0631e5959
mik129 <aaxeldiomande@gmail.com>
2026-09-17 16:24:24 +0200

    Refactor my_strlen function for clarity

--- my_strlen.c
+++ my_strlen.c
@@ -3,11 +3,11 @@
 size_t
 my_strlen(const char *s)
 {
-    size_t n = 0;
+ size_t compteur = 0;
 
-    while (*s) {
-        n++;
-        s++;
-    }
-    return n;
+ while (*s != '\0') {
+ compteur += 1;
+ s++;
+ }
+ return compteur;
 }
